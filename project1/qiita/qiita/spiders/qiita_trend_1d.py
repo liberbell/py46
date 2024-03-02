002100a424987eba0f03757ebc1ee3436f5e03a4
Liberbell <liberbell@gmail.com>
2024-03-02 13:09:28 +0900

    qiita_trend_1d4

--- project1/qiita/qiita/spiders/qiita_trend_1d.py
+++ project1/qiita/qiita/spiders/qiita_trend_1d.py
@@ -12,5 +12,5 @@ class QiitaTrend1dSpider(scrapy.Spider):
         url = response.xpath("//div/main/div[2]/article[1]/h2/a/@href").get()
 
         category = response.css("nav > div > a.style-c221vd::text").get()
-        titles = response.css().get()
-        url = response.css().get()
+        titles = response.css('div > main > div.style-1p44k52 > article:nth-child(1) > h2 > a').get()
+        url = response.css('nav > div > a:first-child::text').get()
