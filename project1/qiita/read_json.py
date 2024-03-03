5a229a68f693903e1ece5fe35636e4ebc99accff
Liberbell <liberbell@gmail.com>
2024-03-03 12:13:25 +0900

    read_json4

--- project1/qiita/read_json.py
+++ project1/qiita/read_json.py
@@ -2,8 +2,12 @@ import json
 
 with open("data.json", encoding="utf-8") as f:
     qiita_data = json.load(f)
-    print(qiita_data[0])
+    # print(qiita_data[0])
     titles = qiita_data[0]["titles"]
-    print(titles)
+    # print(titles)
     urls = qiita_data[0]["urls"]
-    print(urls)
+    # print(urls)
+
+# zip(urls, titles)
+for title, url in zip(titles, urls):
+    print(title, url)
