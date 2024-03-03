24508b229f549bd33a8af3d0cc15dfd167607e81
Liberbell <liberbell@gmail.com>
2024-03-04 08:48:58 +0900

    desktop5

--- project1/yodobashi/yodobashi/spiders/desktop.py
+++ project1/yodobashi/yodobashi/spiders/desktop.py
@@ -4,7 +4,7 @@ import scrapy
 class DesktopSpider(scrapy.Spider):
     name = 'desktop'
     allowed_domains = ['www.yodobashi.com']
-    start_urls = ['https://www.yodobashi.com/category/19531/']
+    start_urls = ['https://www.yodobashi.com/category/19531/11970/34646']
 
     def parse(self, response):
         products = response.xpath("//div/a[contains(@class, 'js_productListPostTag')]")
