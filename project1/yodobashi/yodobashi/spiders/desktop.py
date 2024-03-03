64e55cfa65d6c6bdc5ab2364d80c6bc14f0cdf6e
Liberbell <liberbell@gmail.com>
2024-03-03 13:15:04 +0900

    desktop1

--- project1/yodobashi/yodobashi/spiders/desktop.py
+++ project1/yodobashi/yodobashi/spiders/desktop.py
@@ -3,8 +3,8 @@ import scrapy
 
 class DesktopSpider(scrapy.Spider):
     name = 'desktop'
-    allowed_domains = ['www.yodobashi.com/category/19531']
-    start_urls = ['http://www.yodobashi.com/category/19531/']
+    allowed_domains = ['www.yodobashi.com']
+    start_urls = ['https://www.yodobashi.com/category/19531/']
 
     def parse(self, response):
         pass
