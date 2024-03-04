e7b5b5a29cd20425be1ee39a79c41b694f86c4d5
Liberbell <liberbell@gmail.com>
2024-03-05 08:09:51 +0900

    desktop1

--- project1/yodobashi/yodobashi/spiders/desktop.py
+++ project1/yodobashi/yodobashi/spiders/desktop.py
@@ -9,15 +9,17 @@ class DesktopSpider(scrapy.Spider):
     def parse(self, response):
         products = response.xpath("//div[1]/a[contains(@class, 'js_productListPostTag')]")
         # products = response.css("div > a.js_productListPostTag")
+        names = response.xpath("//a/div/div[2]/div[1]/strong")
+        print(names)
 
-        for product in products:
-            # maker = product.xpath(".//div/div[contains(@class, 'brand')]/text()").get()
-            # maker = product.css("div > div.brand::text")
+        # for product in products:
+        #     # maker = product.xpath(".//div/div[contains(@class, 'brand')]/text()").get()
+        #     # maker = product.css("div > div.brand::text")
 
-            name = product.xpath(".//a/div/div[2]/div[1]/strong/text()").get
-            price = product.xpath(".//div[2]/ul/li[1]/strong[contains(@class, 'red')]/text()").get()
+        #     name = product.xpath(".//a/div/div[2]/div[1]/strong/text()").get
+        #     price = product.xpath(".//div[2]/ul/li[1]/strong[contains(@class, 'red')]/text()").get()
 
-            yield {
-                "name": name,
-                "price": price
-            }
+        #     yield {
+        #         "name": name,
+        #         "price": price
+        #     }
