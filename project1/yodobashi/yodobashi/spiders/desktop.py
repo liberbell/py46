97b0160fbf4a0c79ec4d0fb76732311099e2b246
Liberbell <liberbell@gmail.com>
2024-03-06 08:10:54 +0900

    desktop4

--- project1/yodobashi/yodobashi/spiders/desktop.py
+++ project1/yodobashi/yodobashi/spiders/desktop.py
@@ -10,29 +10,19 @@ class DesktopSpider(scrapy.Spider):
     def parse(self, response):
         logging.info(response.url)
         products = response.xpath("//div[3]/div[contains(@class, 'productListTile')]")
-        # products = response.css("div > a.js_productListPostTag")
-        # brands = response.xpath("//a/div[2][contains(@class, 'pName')]/p[1]/text()").getall()
-        # names = response.xpath("//a/div[2][contains(@class, 'pName')]/p[2]/text()").getall()
 
-        # yield {
-        #     "products": products,
-        #     "brands": brands,
-        #     "names": names
-        # }
+        for product in products:
+            brand = product.xpath(".//a/div[2][contains(@class, 'pName')]/p[1]/text()").get()
+            # maker = product.css("div > div.brand::text")
 
+            name = product.xpath(".//div[contains(@class, 'pName')]/p[2]/text()").get()
+            price = product.xpath(".//ul/li[2]/span[contains(@class, 'productPrice')]/text()").get()
 
-        # for product in products:
-        #     brand = product.xpath(".//a/div[2][contains(@class, 'pName')]/p[1]/text()").get()
-        #     # maker = product.css("div > div.brand::text")
-
-        #     name = product.xpath(".//div[contains(@class, 'pName')]/p[2]/text()").get()
-        #     price = product.xpath(".//ul/li[2]/span[contains(@class, 'productPrice')]/text()").get()
-
-        #     yield {
-        #         "brand": brand,
-        #         "name": name,
-        #         "price": price
-        #     }
+            yield {
+                "brand": brand,
+                "name": name,
+                "price": price
+            }
 
         next_page = response.xpath("//a[@class='next']")
         if next_page:
