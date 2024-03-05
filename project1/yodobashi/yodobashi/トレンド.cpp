63db267187a881973a0055bf37058ef93bbc6781
Liberbell <liberbell@gmail.com>
2024-03-06 08:50:07 +0900

    trend1

---
+++ project1/yodobashi/yodobashi/トレンド.cpp
@@ -0,0 +1,71 @@
+トレンド
+//*[@id="GlobalHeader-react-component-e11666e9-4f59-4257-818d-ac3784856861"]
+//nav/div/a[3]/text()
+//nav/div/a[1]/text()
+
+トレンド
+/html/body/div[1]/div[1]/nav/div/a[3]/text()
+/html/body/div[1]/div[1]/nav/div/a[1]/text()
+nav > div > a.style-c221vd::text
+
+category = response.css('nav > div > a:first-child::text')
+category = response.css("/html/body/div[1]/div[1]/nav/div/a[1]/text()")
+
+#GlobalHeader-react-component-5bf49318-2565-4702-b3d4-fad80b6be505 > nav > div > a.style-c221vd
+
+
+1つめ
+//*[@id="HomeTrendPage-react-component-b128b9b4-c37f-47de-9ec2-3a0001a08f81"]/div/main/div[2]/article[1]/h2/a
+//*[@id="HomeTrendPage-react-component-b128b9b4-c37f-47de-9ec2-3a0001a08f81"]/div/main/div[2]/article[1]/h2/a
+#HomeTrendPage-react-component-b128b9b4-c37f-47de-9ec2-3a0001a08f81 > div > main > div.style-1p44k52 > article:nth-child(1) > h2 > a
+/html/body/div[1]/div[2]/div/div/main/div[2]/article[1]/h2/a
+//*[@id="HomeTrendPage-react-component-f7cad61f-2ce7-49c0-9382-7147d777f9af"]/div/main/div[2]/article[1]/h2/a
+//nav/div/a[1]/text()
+
+#HomeTrendPage-react-component-f7cad61f-2ce7-49c0-9382-7147d777f9af > div > main > div.style-1p44k52 > article:nth-child(1) > h2 > a
+h2 > a::text
+
+URL
+//div/main/div[2]/article[1]/h2/a/@href
+div > main > div.style-1p44k52 > article:nth-child(1) > h2 > a::attr(href)
+nav > div > a:first-child::text'
+h2 > a::attr(href)
+
+よどばし
+//*[@id="contents"]/div[7]/div[1]/div[1]/a
+//div[1]/a[contains(@class, "js_productListPostTag")]
+/html/body/div[2]/div[2]/div[7]/div[1]/div[1]/a
+//*[@id="contents"]/div[7]/div[1]/div[1]
+//*[@id="js_carousel_criteo_product1"]/div/div/div/div/div[1]/div/div/a/div[2]/div[2]
+//div/a[contains(@class, "js_productListPostTag")]
+/html/body/div[1]/div[5]/div/div[1]/div/div[3]/div[3]/div[1]
+//div[3]/div[1][contains(@class, 'productListTile')]
+div > a.js_productListPostTag
+
+maker
+/html/body/div[1]/div[5]/div/div[1]/div/div[3]/div[3]/div[1]/a/div[2]/p[1]
+//a/div[2][contains(@class, 'pName')]/p[1]
+
+//div/div[contains(@class, "brand")]/text()
+div > div.brand::text
+//div/div[contains(@class, "productName")]/text()
+/html/body/div[2]/div[2]/div[7]/div[1]/div[1]/a/div/div[2]/div[1]/strong
+//a/div/div[2]/div[1]/strong/text()
+//a/div[2][contains(@class, 'pName')]/p[2]
+//div[contains(@class, 'pName')/p[2]/text()
+//div[contains(@class, 'pName')]/p[2]/text()
+
+div > div.js_productName::text
+//div/ul/li/strong[contains(@class, "js_ppSalesPrice")]/text()
+/html/body/div[2]/div[2]/div[7]/div[1]/div[1]/a/div/div[2]/div[2]/ul/li[1]
+/html/body/div[1]/div[5]/div/div[1]/div/div[3]/div[3]/div[1]/div[3]/ul/li[2]/span
+//ul/li[2]/span[contains(@class, 'productPrice')]/text()
+
+//div[2]/ul/li[1]/strong[contains(@class, 'red')]/text()
+li > strong.js_ppSalesPrice::text
+
+next
+/html/body/div[1]/div[5]/div/div[1]/div/div[3]/div[4]/ul/li[5]/a
+//a[@class='next']/@href
+//li/a[contains(@class, 'next')]/@href
+a.next::attr(href)
