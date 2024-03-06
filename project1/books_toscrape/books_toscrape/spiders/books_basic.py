bffc1416a3da5919027dcf6da8d0de0388f2a19f
Liberbell <liberbell@gmail.com>
2024-03-07 08:25:08 +0900

    books_basic6

--- project1/books_toscrape/books_toscrape/spiders/books_basic.py
+++ project1/books_toscrape/books_toscrape/spiders/books_basic.py
@@ -7,18 +7,18 @@ class BooksBasicSpider(scrapy.Spider):
     start_urls = ['https://books.toscrape.com/catalogue/category/books/fantasy_19/index.html']
 
     def parse(self, response):
-        # books = response.xpath("//li/article[@class='product_pod']")
-        books = response.xpath("//h3")
-
-        yield {
-            "books": books
-        }
-
-        # for book in books:
-        #     book_title = response.xpath(".//article/h3/a/@title").get()
-        #     url = response.xpath(".//article/h3/a/@href").get()
+        books = response.xpath("//li/article[@class='product_pod']")
+        # books = response.xpath("//h3")
 
         # yield {
-        #     "book_title": book_title,
-        #     "url": url,
+        #     "books": books
         # }
+
+        for book in books:
+            book_title = response.xpath(".//article/h3/a/@title").get()
+            url = response.xpath(".//article/h3/a/@href").get()
+
+            yield {
+                "book_title": book_title,
+                "url": url,
+            }
