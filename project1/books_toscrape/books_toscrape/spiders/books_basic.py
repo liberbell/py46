9a39120182419a047d8334f375351e032dd3b561
Liberbell <liberbell@gmail.com>
2024-03-06 09:08:07 +0900

    books_basic2

--- project1/books_toscrape/books_toscrape/spiders/books_basic.py
+++ project1/books_toscrape/books_toscrape/spiders/books_basic.py
@@ -3,8 +3,8 @@ import scrapy
 
 class BooksBasicSpider(scrapy.Spider):
     name = 'books_basic'
-    allowed_domains = ['books.toscrape.com/catalogue/category/books/fantasy_19/index.html']
-    start_urls = ['http://books.toscrape.com/catalogue/category/books/fantasy_19/index.html/']
+    allowed_domains = ['books.toscrape.com']
+    start_urls = ['https://books.toscrape.com/catalogue/category/books/fantasy_19/index.html']
 
     def parse(self, response):
-        pass
+        books = response.xpath("//li/article[@class='product_pod']")
