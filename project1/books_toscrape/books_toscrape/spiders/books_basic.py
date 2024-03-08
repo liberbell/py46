b735537a5fab2041ea1dd582e4218b76f89979d2
Liberbell <liberbell@gmail.com>
2024-03-08 10:45:56 +0900

    books_basic7

--- project1/books_toscrape/books_toscrape/spiders/books_basic.py
+++ project1/books_toscrape/books_toscrape/spiders/books_basic.py
@@ -35,10 +35,12 @@ class BooksBasicSpider(scrapy.Spider):
     def parse_item(self, response):
         book_details = response.xpath("//div[@class='col-sm-6 product_main']")
         title = response.xpath(".//h1/text()").get()
-        price = response.xpath(".//p[@class="price_color"]/text()").get()
+        price = response.xpath(".//p[@class='price_color']/text()").get()
+        stock = response.xpath(".//p[contains(@class, 'star-rating')]/@class").get()
 
         yield {
             "title": title,
             "price": price,
+            "stock": stock,
         }
 
