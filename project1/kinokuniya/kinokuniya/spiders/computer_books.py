04d6b2ebe3900688f0a709d88c1bfaaf8705eeb2
Liberbell <liberbell@gmail.com>
2024-03-09 12:45:38 +0900

    computer_books4

--- project1/kinokuniya/kinokuniya/spiders/computer_books.py
+++ project1/kinokuniya/kinokuniya/spiders/computer_books.py
@@ -12,6 +12,8 @@ class ComputerBooksSpider(CrawlSpider):
     rules = (
         Rule(LinkExtractor(restrict_xpaths="//h3[@class='heightLine-2']/a"),
              callback='parse_item', follow=False),
+        Rule(LinkExtractor(restrict_xpaths="(//a[contains(text(), '次へ')])[1]"),
+             callback='parse_item', follow=False),
     )
 
     def parse_item(self, response):
