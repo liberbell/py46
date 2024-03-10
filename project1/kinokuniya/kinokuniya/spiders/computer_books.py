d62eeaca5a7f2113522828ec4b1623b71f91eafc
Liberbell <liberbell@gmail.com>
2024-03-10 13:30:23 +0900

    computer_books7

--- project1/kinokuniya/kinokuniya/spiders/computer_books.py
+++ project1/kinokuniya/kinokuniya/spiders/computer_books.py
@@ -18,12 +18,12 @@ class ComputerBooksSpider(CrawlSpider):
 
     def parse_item(self, response):
         logging.info(response.url)
-        title = response.xpath("//h3/text()").get()
+        title = response.xpath("//h3/text()").getall()
         author = response.xpath("//div[@class='infobox ml10 mt10']/ul/li[1]/a/text()").get()
         price = response.xpath("//span[@class='redhot st']/text()").get()
         publisher = response.xpath("//a[contains(@href, 'publisher-key')]/text()").get()
-        size = response.xpath("//ul[@class='dotted mt05 pt05']/li/text()").get()
-        pages = response.xpath("//ul[@class='dotted mt05 pt05']/li/text()").get()
+        size = response.xpath("normalize-space(//ul[@class='dotted mt05 pt05']/li/text())").get()
+        pages = response.xpath("normalize-space(//ul[@class='dotted mt05 pt05']/li/text())").get()
         isbn = response.xpath("//li[@itemprop='identifier']/text()").get()
 
         yield {
