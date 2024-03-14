86bf01e9aa8fbeff7c46de869014d72b3206a417
Liberbell <liberbell@gmail.com>
2024-03-14 16:52:22 +0900

    books_crawl2

--- project1/books_toscrape/books_toscrape/spiders/books_crawl.py
+++ project1/books_toscrape/books_toscrape/spiders/books_crawl.py
@@ -9,7 +9,8 @@ class BooksCrawlSpider(CrawlSpider):
     start_urls = ['https://books.toscrape.com/catalogue/category/books/fantasy_19/index.html']
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(restrict_xpaths="//h3/a/@href"), callback='parse_item', follow=False),
+        Rule(LinkExtractor(restrict_xpaths="//li[@class='next']/a/@href"))
     )
 
     def parse_item(self, response):
