f0ac4980d3f06e16dfb1512900a6cfba2b99b532
Liberbell <liberbell@gmail.com>
2024-03-14 16:42:07 +0900

    books_crawl1

---
+++ project1/books_toscrape/books_toscrape/spiders/books_crawl.py
@@ -0,0 +1,20 @@
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+
+class BooksCrawlSpider(CrawlSpider):
+    name = 'books_crawl'
+    allowed_domains = ['books.toscrape.com/catalogue/category/books/fantasy_19/index.html']
+    start_urls = ['http://books.toscrape.com/catalogue/category/books/fantasy_19/index.html/']
+
+    rules = (
+        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
+        item = {}
+        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
+        #item['name'] = response.xpath('//div[@id="name"]').get()
+        #item['description'] = response.xpath('//div[@id="description"]').get()
+        return item
