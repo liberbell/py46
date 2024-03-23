86e290f49381b198b9cb6564a7a8a7418b14bfc3
Liberbell <liberbell@gmail.com>
2024-03-23 23:02:48 +0900

    selenium_test7

--- project1/selenium/selenium_test.py
+++ project1/selenium/selenium_test.py
@@ -6,3 +6,6 @@ driver.get("https://www.bing.com/")
 
 search_bar = driver.find_element_by_id("sb_form_q")
 search_bar.send_keys("python")
+
+search_btn = driver.find_element_by_xpath("//label[@for='sb_form_q']")
+search_btn.click()
