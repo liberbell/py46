ec5fb2441d2e033d895d1bdd7d023a07b728e003
Liberbell <liberbell@gmail.com>
2024-03-25 08:57:38 +0900

    google_python1

--- project1/google/google/google/spiders/google_python.py
+++ project1/google/google/google/spiders/google_python.py
@@ -1,10 +1,13 @@
 import scrapy
-
+from scrapy_selenium import SeleniumRequest
 
 class GooglePythonSpider(scrapy.Spider):
     name = "google_python"
     allowed_domains = ["www.google.co.jp"]
     start_urls = ["https://www.google.co.jp"]
+
+    def start_requests(self):
+
 
     def parse(self, response):
         pass
