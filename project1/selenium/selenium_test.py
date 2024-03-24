c7cbbe9c14f8f7e65197cd04b754d87a3a4bdebc
Liberbell <liberbell@gmail.com>
2024-03-24 22:49:54 +0900

    selenium_test4

--- project1/selenium/selenium_test.py
+++ project1/selenium/selenium_test.py
@@ -13,4 +13,6 @@ search_btn = driver.find_element_by_xpath("//label[@for='sb_form_go']")
 # search_btn.click()
 search_bar.submit()
 
+sleep(2)
+driver.find_elements_by_xpath("//div[@class='b_title']/h2/a")
 # /html/body/div[2]/div/div[3]/div[2]/form/label/svg/path
