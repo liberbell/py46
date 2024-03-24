501472fba21404f364718c8ca31a215c891f5775
Liberbell <liberbell@gmail.com>
2024-03-24 23:10:49 +0900

    selenium_test13

--- project1/selenium/selenium_test.py
+++ project1/selenium/selenium_test.py
@@ -19,11 +19,17 @@ search_btn = driver.find_element_by_xpath("//label[@for='sb_form_go']")
 # search_btn.click()
 search_bar.submit()
 
-sleep(2)
-for elem in driver.find_elements_by_xpath("//h2[not(contains(@class,'b_topTitleAd'))]/a"):
-    print(elem.text)
-    print(elem.get_attribute("href"))
+i = 0
+while True:
+    sleep(2)
+    for elem in driver.find_elements_by_xpath("//h2[not(contains(@class,'b_topTitleAd'))]/a"):
+        print(elem.text)
+        print(elem.get_attribute("href"))
 
-next_link = driver.find_element_by_xpath("//a[@title='次のページ']")
-driver.get(next_link.get_attribute("href"))
+    next_link = driver.find_element_by_xpath("//a[@title='次のページ']")
+    driver.get(next_link.get_attribute("href"))
+    i += 1
+    if 1 > 4:
+        break
+
 # /html/body/div[2]/div/div[3]/div[2]/form/label/svg/path
