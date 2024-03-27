59228d2c6cdb66c9c1e344289430b3111ffc31be
Liberbell <liberbell@gmail.com>
2024-03-28 08:40:58 +0900

    luxury_watch2

--- project1/antenna/antenna/spiders/luxury_watch.py
+++ project1/antenna/antenna/spiders/luxury_watch.py
@@ -1,5 +1,6 @@
 import scrapy
 from scrapy_selenium import SeleniumRequest
+from time import sleep
 
 class LuxuryWatchSpider(scrapy.Spider):
     name = "luxury_watch"
@@ -16,7 +17,10 @@ class LuxuryWatchSpider(scrapy.Spider):
         driver = response.meta["driver"]
 
         search_text = driver.find_element_by_xpath("//input[@id='search-input']")
+        search_text.send_keys("高級時計")
+
         search_button = driver.find_element_by_xpath("//input[@id='search-button']")
+        search_button.submit()
 
         w = driver.execute_script("return document.body.scrollWidth")
         h = driver.execute_script("return document.body.scrollHeight")
